03d294feee554780e7d7f1fe1ce6760c1d908b18
Brandon Daniel <brandonjdaniel96@gmail.com>
2018-01-25 20:13:37 -0500

    removed hide animation
    Removing animation and returned back to document ready block.

--- homeFunctionality.js
+++ homeFunctionality.js
@@ -1,18 +1,20 @@
 // Fade in functionality on page load
 // load in each item with its successor loading after
+$(document).ready(function() {
 
+   let fadeInHeader = function() { $("div.jumbotron").fadeIn(1000); };
+   let fadeInArticle = function() { $("article").fadeIn(2000); };
+   let fadeInFooter = function() { $("footer").fadeIn(2500); };
 
-let fadeInHeader = function() { $("div.jumbotron").fadeIn(1000); };
-let fadeInArticle = function() { $("article").fadeIn(2000); };
-let fadeInFooter = function() { $("footer").fadeIn(2500); };
+   hideFadeIns();
+   setTimeout(fadeInHeader, 100);
+   setTimeout(fadeInArticle, 200);
+   setTimeout(fadeInFooter, 300);
+
+});
 
 function hideFadeIns() {
-   $("div.jumbotron").hide();
-   $("article").hide();
-   $("footer").hide();
+   $("div.jumbotron").hide(0);
+   $("article").hide(0);
+   $("footer").hide(0);
 }
-
-hideFadeIns();
-setTimeout(fadeInHeader, 100);
-setTimeout(fadeInArticle, 200);
-setTimeout(fadeInFooter, 300);
